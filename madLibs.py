60f81f5cf8f08ea1b47b3148ff16eb4dd37a0183
EconC1ass <bizmail327@gmail.com>
2018-09-05 13:05:35 -0700

    Started test suite

--- madLibs.py
+++ madLibs.py
@@ -13,17 +13,25 @@ my mother called. I couldn't wait to get my _______
 on that _______ Thanksgiving meal.'''
 print (blank)
 
-noun = str(input('Enter a noun: '))
-person_in_room = str(input('Enter the name of a person in the room: '))
-verb = str(input('Enter a verb: '))
-part_of_body_plural = str(input('Enter part of the body (plural): '))
-adjective = str(input('Enter an adjective: '))
+noun = test_str(raw_input('Enter a noun: '))
+person_in_room = test_str(raw_input('Enter the name of a person in the room: '))
+verb = test_str(raw_input('Enter a verb: '))
+part_of_body_plural = test_str(raw_input('Enter part of the body (plural): '))
+adjective = test_str(raw_input('Enter an adjective: '))
 
 list = [noun, person_in_room, verb, part_of_body_plural, adjective]
 
 # def spellCheck(list_words):
 #     for item in list_words:
 #         testInputs(item)
+
+def test_str(prompt):
+    try:
+        string = str(prompt)    
+        return True
+    except ValueError:
+        print("Not a string!")
+        return False
 
 story = '''It was Thanksgiving, and the scent of succulent roast {}
 wafted through my house. "{}, it's time to {}!"
