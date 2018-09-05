fe66a4e01ada925d62950f9ee4d1205e515e4f7b
EconC1ass <bizmail327@gmail.com>
2018-09-05 12:42:22 -0700

    Base Mad Libs model created

--- madLibs.py
+++ madLibs.py
@@ -1,15 +1,17 @@
-import unittest
-# def ask_input():
-# random.coice(input_list)
+# import unittest
+#
+# class testInputs(word):
+#     def test_spell(word):
+#         if word in words:
+#             return True
+#         else:
+#             print("Wrong spelling.")
 
-class testInputs():
-    def test_word(input):
-        if integer in input:
-            print("Invalid input!")
-        else:
-            return True
-
-    # def test_noun():
+blank ='''It was Thanksgiving, and the scent of succulent roast _______
+wafted through my house. "_______, it's time to _______!"
+my mother called. I couldn't wait to get my _______
+on that _______ Thanksgiving meal.'''
+print (blank)
 
 noun = str(input('Enter a noun: '))
 person_in_room = str(input('Enter the name of a person in the room: '))
@@ -17,10 +19,16 @@ verb = str(input('Enter a verb: '))
 part_of_body_plural = str(input('Enter part of the body (plural): '))
 adjective = str(input('Enter an adjective: '))
 
+list = [noun, person_in_room, verb, part_of_body_plural, adjective]
+
+# def spellCheck(list_words):
+#     for item in list_words:
+#         testInputs(item)
+
 story = '''It was Thanksgiving, and the scent of succulent roast {}
 wafted through my house. "{}, it's time to {}!"
 my mother called. I couldn't wait to get my {}
 on that {} Thanksgiving meal.'''.format(noun, person_in_room, verb, part_of_body_plural, adjective)
 
-
+# spellCheck(list)
 print(story)
